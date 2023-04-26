76e5f8761305b2cfaeacd89fe56f90e6281ac24f
Tamer Ihab <tamerihab02@gmail.com>
2023-04-26 22:52:02 +0200

    Editied Something in main.cpp

--- trie.cpp
+++ trie.cpp
@@ -1,40 +1,42 @@
-#include"trie.h"
+#include "trie.h"
 
-trie::trie():count(0) {
+trie::trie() : count(0)
+{
 	root = new Node('\0');
 }
 
+void trie::insert(std::string str)
+{
 
+	Node *cur = root;
 
-void trie::insert(std::string str) {
+	for (int index = 0; index < str.length(); index++)
+	{
+		if (!cur->child[str[index]])
+		{
+			Node *newNode = new Node(str[index]);
+			cur->child[str[index]] = newNode;
+		}
 
-	Node* cur=root;
-
-	for (int index = 0; index < str.length(); index++) {
-		 if (!cur->child[str[index]]) {
-			 Node* newNode = new Node(str[index]);
-			 cur->child[str[index]] = newNode;
-		 }
-		
 		cur = cur->child[str[index]];
 	}
 	cur->is_leaf = true;
-	
+
 	count++;
-	
 }
 
 //-------------------------------------------------------------------
 
+bool trie::search(std::string str)
+{
 
-bool trie::search(std::string str) {
-	
-	Node* cur =root;
+	Node *cur = root;
 
 	if (!cur)
 		return false;
 
-	for (int index = 0; index < str.length(); index++) {
+	for (int index = 0; index < str.length(); index++)
+	{
 		if (!cur->child[str[index]])
 			return false;
 		cur = cur->child[str[index]];
@@ -42,7 +44,4 @@ bool trie::search(std::string str) {
 	return cur->is_leaf;
 }
 
-
 //-------------------------------------------------------------------
-
-
--- trie.h
+++ trie.h
@@ -1,49 +1,45 @@
 
-#include<iostream>
+#include <iostream>
 
-#include<vector>
+#include <vector>
 
-#include<cstring>
-
-
+#include <cstring>
 
 using namespace std;
 
-
-class trie {
+class trie
+{
 
 private:
-
-
-	class Node {
+	class Node
+	{
 
 	public:
-
 		char data;
 
 		bool is_leaf{};
 
-		Node* child[128];
+		Node *child[128];
 
-		Node(char data) :is_leaf(false) {
+		Node(char data) : is_leaf(false)
+		{
 
-			this->data = data; 
+			this->data = data;
 			memset(child, 0, sizeof(child));
-
 		}
 	};
 
-
-	Node* root;
+	Node *root;
 
 	long count{};
 
+	bool hasChild(Node *root)
+	{
 
-
-	bool hasChild(Node* root) {
-
-		for (int i = 0; i < 128; ++i) {
-			if (root->child[i] != NULL) {
+		for (int i = 0; i < 128; ++i)
+		{
+			if (root->child[i] != NULL)
+			{
 				return true;
 			}
 		}
@@ -55,30 +51,35 @@ private:
 	 Postcondition: Returns true if there is a child, false if not..
 	 --------------------------------------------------------------------*/
 
-
-	void displayUtil(string s, Node* root, std::vector<std::string>& res) {
+	void displayUtil(string s, Node *root, std::vector<std::string> &res)
+	{
 		// if the given char is end of word print it
 
-		if (root->is_leaf == true) {
+		if (root->is_leaf == true)
+		{
 			cout << s << endl;
 
 			res.push_back(s);
-			
 		}
 		// for all children in root if not NULL recursively call its children
-		for (int i = 0; i < 128; i++) {
-			if (root->child[i] != NULL) {
-				displayUtil(s + root->child[i]->data, root->child[i],res);
+		for (int i = 0; i < 128; i++)
+		{
+			if (root->child[i] != NULL)
+			{
+				displayUtil(s + root->child[i]->data, root->child[i], res);
 			}
 		}
 	}
 
-	void autoSuggestUtil(string input, Node* root, std::vector<std::string>& res) {
-		Node* cur = root;
+	void autoSuggestUtil(string input, Node *root, std::vector<std::string> &res)
+	{
+		Node *cur = root;
 		// traversing the given input string
-		for (int i = 0; i < input.length(); i++) {
+		for (int i = 0; i < input.length(); i++)
+		{
 			// no such prefix exist then return
-			if (cur->child[input[i]] == NULL) {
+			if (cur->child[input[i]] == NULL)
+			{
 				cout << input << endl;
 				return;
 			}
@@ -86,16 +87,17 @@ private:
 		}
 
 		// if it is end node and has no children print the input string
-		if (cur->is_leaf and hasChild(cur) == false) {
+		if (cur->is_leaf and hasChild(cur) == false)
+		{
 			cout << input << endl;
-			return ;
+			return;
 		}
 
 		// print the subtree
-		if (hasChild(root)) {
-			displayUtil(input, cur,res);
+		if (hasChild(root))
+		{
+			displayUtil(input, cur, res);
 		}
-
 	}
 	/*--------------------------------------------------------------------
 	 Completes the string given with all possible words available
@@ -104,10 +106,7 @@ private:
 	 provided string is valid..
 	 --------------------------------------------------------------------*/
 
-
-
 public:
-
 	trie();
 	/*--------------------------------------------------------------------
 	 Default constructor: builds an empty trie object.
@@ -124,32 +123,32 @@ public:
 
 	bool search(std::string str);
 	/*--------------------------------------------------------------------
-	 Searches if word exists in trie.
-	 Precondition:  string is a valid string
-	 Postcondition:returns true if found otherwise it returns false,
-	 provided string is valid..
-	 --------------------------------------------------------------------*/
+	Searches if word exists in trie.
+	Precondition:  string is a valid string
+	Postcondition:returns true if found otherwise it returns false,
+	provided string is valid..
+	--------------------------------------------------------------------*/
 
-	void display(std::vector<std::string>& res) {
-		displayUtil("", root,res);
+	void display(std::vector<std::string> &res)
+	{
+		displayUtil("", root, res);
 	}
 
 	/*--------------------------------------------------------------------
 	prints the Strings in the trie
-   Precondition:Vector is a valid vector.
-   Postcondition: all strings in vector has been returned..
-   --------------------------------------------------------------------*/
+	Precondition:Vector is a valid vector.
+	Postcondition: all strings in vector has been returned..
+	--------------------------------------------------------------------*/
 
-
-	void autoComplete(string input, std::vector<std::string>& res) {
-		return autoSuggestUtil(input, root,res);
+	void autoComplete(string input, std::vector<std::string> &res)
+	{
+		return autoSuggestUtil(input, root, res);
 	}
 
 	/*--------------------------------------------------------------------
-   Completes the string given with all possible words available
-   Precondition:  string is a valid string.
-   Postcondition: all words with the same prefix as the string has been returned,
-   provided string is valid..
-   --------------------------------------------------------------------*/
+	Completes the string given with all possible words available
+	Precondition:  string is a valid string.
+	Postcondition: all words with the same prefix as the string has been returned,
+	provided string is valid..
+	--------------------------------------------------------------------*/
 };
-
